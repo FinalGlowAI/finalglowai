52b1984eee69da820c375ed98de4a996287a724c
FinalGlowAI <weehab.community@gmail.com>
2026-06-12 21:44:11 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,6 +4,12 @@ const config: CapacitorConfig = {
   appId: 'com.finalglow.app',
   appName: 'finalglowai',
   webDir: 'dist',
+  android: {
+    allowMixedContent: true,
+  },
+  server: {
+    androidScheme: 'https',
+  },
 };
 
 export default config;
